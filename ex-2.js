c9d4befe1e2bb33285d91b1e81bd727ba181283c
Dennis <suwatchai.kaew@gmail.com>
2023-12-08 10:47:53 +0700

    feat: add finished ex1.2 v1

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,17 @@
 //Exercise 2: Valid Palindrome
+String.prototype.eiei = function () {
+  return this.replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .join("");
+};
 
 const isPalindrome = function (s) {
   //Start Coding here
+  let newStr = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let newStrReverse = newStr.eiei();
+  return newStr === newStrReverse ? true : false;
 };
 
 const result1 = isPalindrome("A man, a plan, a canal: Panama");
